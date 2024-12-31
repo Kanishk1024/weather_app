eb658a4820f95be06db1b104894d81004480e2d5
Kanishk1024 <154776366+Kanishk1024@users.noreply.github.com>
2024-12-31 14:42:01 +0530

    Light Mode

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(useMaterial3: true),
+      theme: ThemeData(useMaterial3: true),
       title: "WEATHER APP",
       home: const WeatherScreen(),
     );
